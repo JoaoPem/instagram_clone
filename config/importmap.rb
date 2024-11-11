78e5e332c23a242fe78af9ee640772f1db271865
João Pedro <devjoaomonteiro@gmail.com>
2024-11-11 13:01:15 -0300

    Post Adjust

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://unpkg.com/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js"
+pin "filepond" # @4.32.1
+pin_all_from "app/javascript/custom", under: "custom"
+pin "filepond-plugin-image-preview" # @4.6.12
