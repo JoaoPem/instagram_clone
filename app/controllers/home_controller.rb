c4a7e9a0ce700c604bb15cdf5dca581f4cf37a86
João Pedro <devjoaomonteiro@gmail.com>
2024-11-19 11:30:31 -0300

    adding arrows to the edges of vertices

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -48,31 +48,4 @@ class HomeController < ApplicationController
   def set_feeds
     @feeds = Post.where(user: [ current_user, current_user.followings ].flatten).order(created_at: :desc)
   end
-
-
-
-
-  # IDEIA 1
-  # def set_suggestions
-  #   @suggestions = [current_user.followers]
-  #   [current_user.followers, current_user.followings].flatten.uniq.each do |f|
-  #     @suggestions.append([f.followers, f.followings])
-  #   end
-  #   @suggestions = [@suggestions, User.all.sample(10)].flatten.uniq - [current_user.followings, current_user].flatten
-  #   @suggestions = @suggestions.sample(5)
-  # end
-
-
-  # IDEIA 2
-  # def set_suggestions
-
-  #   @suggestions = current_user.followers.flat_map do |follower|
-  #     follower.followers + follower.followings
-  #   end
-
-  #   @suggestions = @suggestions.uniq - current_user.followings - [current_user]
-
-  #   @suggestions = @suggestions.sample(5)
-
-  # end
 end
